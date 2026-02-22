d2c1ac8d71cf00f7f045ce3d238b675bbdfef366
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-22 15:44:52 -0500

    added

--- to-dolist.py
+++ to-dolist.py
@@ -9,3 +9,7 @@ to_do_list.remove("Call mom")
 print(to_do_list)
 to_do_list[1]="Finish project report and send email"
 print(to_do_list)
+if "Exercise for 30 minutes" in to_do_list:
+    print("Don't forget to exercise!")
+else:
+    print("You have completed your exercise for the day!")
