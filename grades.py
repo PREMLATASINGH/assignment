2a6744b6ccb4d3e036adf8f5bb2d3a86e3a67eda
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-23 15:46:24 -0500

    added

--- grades.py
+++ grades.py
@@ -1,3 +1,6 @@
 grades={78, 85, 92, 88, 95, 80, 82, 90}
 # Calculate the average grade
 average_grade = sum(grades) / len(grades)
+sum_of_grades = sum(grades)
+print(sum_of_grades)
+print(len(grades))
